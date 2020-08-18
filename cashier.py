805d91ccf5bb0d596ef04e87e133b65ad50efbde
Mariam <mariam.elgaria98@gmail.com>
2020-08-18 10:01:00 +0300

    cashier is eazyyyyy

--- cashier.py
+++ cashier.py
@@ -1,5 +1,25 @@
 def main():
-	# write code here
+        item=input("Item (enter \"done\" when finished): ")
+        rec=[]
+        while(not(item =="done")):
+                d={}
+                
+                d["name"]=item
+                
+                price=float(input("Price: "))
+                d["price"]=price
+                quantity=int(input("Quantity: "))
+                d["quantity"]=quantity
+                rec.append(d)
+                item=input("Item (enter \"done\" when finished): ")
+                
+        total=0
+        for i in rec:
+                item_price=i["price"]*i["quantity"]
+                print(i["quantity"],i["name"],"%sKD"%(format(item_price,".3f")))
+                
+                total=total+item_price
+        print("Total Price: ",total)
 
 if __name__ == '__main__':
 	main()
